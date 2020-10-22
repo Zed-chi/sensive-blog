07e4b5992936d898c47751af10589f00612e1183
zed-chi <zed-chi@mail.ru>
2020-10-22 12:05:59 +0300

    show popular posts on main page

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.db.models import Count
 from blog.models import Comment, Post, Tag
 
 
@@ -28,8 +29,8 @@ def serialize_tag(tag):
 
 
 def index(request):
-
-    most_popular_posts = []  # TODO. Как это посчитать?
+    most_popular_posts = sorted(Post.objects.all(), key=get_likes_count, reverse=True)[:5]    
+    #most_popular_posts = Post.objects.all().annotate(likes_count=Count('likes')).order_by("-likes_count")[:5]
 
     fresh_posts = Post.objects.order_by('published_at')
     most_fresh_posts = list(fresh_posts)[-5:]
@@ -111,3 +112,7 @@ def contacts(request):
     # позже здесь будет код для статистики заходов на эту страницу
     # и для записи фидбека
     return render(request, 'contacts.html', {})
+
+
+def get_likes_count(post):
+    return post.likes.count()
