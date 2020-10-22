1eb9229ba793186d664d399c6ff91a12bac4783a
zed-chi <zed-chi@mail.ru>
2020-10-22 13:36:41 +0300

    Optimize popular posts

--- blog/views.py
+++ blog/views.py
@@ -29,10 +29,8 @@ def serialize_tag(tag):
 
 
 def index(request):
-    most_popular_posts = sorted(
-        Post.objects.all(), key=get_likes_count, reverse=True
-    )[:5]
-    # most_popular_posts = Post.objects.all().annotate(likes_count=Count('likes')).order_by("-likes_count")[:5]
+    #most_popular_posts = sorted(Post.objects.all(), key=get_likes_count, reverse=True)[:5]
+    most_popular_posts = Post.objects.annotate(likes_count=Count('likes')).order_by("-likes_count")[:5]
 
     fresh_posts = Post.objects.order_by("published_at")
     most_fresh_posts = list(fresh_posts)[-5:]
